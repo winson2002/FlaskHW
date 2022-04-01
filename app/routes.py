2b8ca746dd9003cb5e5ead1e5feffab3453d7c1f
Winson Ma <winson.ma@sjsu.edu>
2022-03-31 23:29:37 -0700

    Edit6

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import myobj
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
+
 class SubmitForm(FlaskForm):
 	city_name = StringField('city_name')
 	submit = SubmitField('Submit')
@@ -12,6 +13,6 @@ city_names = ['Paris', 'London', 'Rome', 'Tahiti']
 @myobj.route('/', methods = ['GET', 'POST'])
 def home():
 	form = SubmitForm()
-	if request.method == 'POST':
+	if form.validate_on_submit():
         	flash(format(form.city_name.data))
 	return render_template('home.html', name=name, city_names=city_names, form=form)
