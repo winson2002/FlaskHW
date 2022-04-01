a596ebb48138b80a32ac7b19d3be88da5b2dd1c2
Winson Ma <winson.ma@sjsu.edu>
2022-03-31 22:44:36 -0700

    secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask, flash, redirect, render_template, request
+from config import Config
 
 myobj = Flask(__name__)
+app.config.from_mapping(
+	SECRET_KEY = 'you-will-never-guess'
+)
 
 from app import routes
